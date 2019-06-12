47c2d1fe55b72bf1ec8b29bc6752ab77ebaba481
Brume Fred <fredbrume23@hotmail.com>
2019-06-12 14:21:02 -0500

    Added Project and a single testcase for location

---
+++ PizzaBoxSolution/PizzaboxTest/Test/Location.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using PizzaBox.Data.Model;
+
+
+namespace PizzaboxTest.Test
+{
+    [TestFixture]
+    class Location
+    {
+        [Test]
+        public void TestLocationData()
+        {
+            Repository repo = new Repository();
+            int locationId = 1;
+            PizzaBox.Domain.Model.Plocation location = repo.GetLocation(locationId);
+
+            string actualCity = location.City;
+            string expectedCityValue = "Brookyln";
+
+            string actualStreet = location.Street; 
+            string expectedStreetValue = "14141 E98ST";
+
+            string actualState = location.PState;
+            string expectedStateValue = "NY";
+
+            string actualZipcode = location.Zipcode;
+            string expectedZipcodeValue = "11236";
+
+            Assert.AreEqual(actualCity, expectedCityValue);
+            Assert.AreEqual(actualStreet, expectedStreetValue);
+            Assert.AreEqual(actualState, expectedStateValue);
+            Assert.AreEqual(actualZipcode, expectedZipcodeValue);
+
+        }
+
+    }
+}
